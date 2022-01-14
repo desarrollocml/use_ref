1e47f95ac5339c3c33b911f76417c1eb28517a77
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-13 21:54:08 -0300

    focus con use ref

--- src/FocusApp.js
+++ src/FocusApp.js
@@ -1,29 +1,34 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function FocusApp() {
   const [text, setText] = useState("");
+  const inputRef = useRef();
 
   function handleText(e) {
     setText(e.target.value);
   }
   function handleFocus(e) {
-    const input = document.getElementById("input");
-    console.log(input.value)
+    const input = inputRef.current;
+   /*  console.log(input.value);
+    input.value = "Valor mutado" */
+    input.focus()
+    /* const input = document.getElementById("input");
+    console.log(input.value);
+    input.value = "Texto mutado";
+    input.focus(); */
   }
 
   return (
     <div>
       <input
-      id="input"
-       type="text" 
-       value={text}
-       onChange={handleText}
-       ></input>
-      <button
-        onClick={handleFocus}
-      >Focus</button>
+        ref={inputRef}
+        id="input"
+        type="text"
+        value={text}
+        onChange={handleText}
+      ></input>
+      <button onClick={handleFocus}>Focus</button>
       <h1>{text}</h1>
     </div>
-
   );
 }
